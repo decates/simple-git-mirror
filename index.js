1320dc8ff21fd3b570eccf5bfbbcad5ccf0315b1
Mark de Cates <decates@gmail.com>
2015-07-27 10:59:35 +0100

    CHORE: Tidy up message

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var http = require('http');
 
 var action = require("./action.js");
 
+var portNumber = 8080;
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Got it.\n');
   action.run();
-}).listen(8080);
-console.log('Server running at http://127.0.0.1:8080/');
+}).listen(portNumber);
+console.log('Server running on port ' + portNumber);
